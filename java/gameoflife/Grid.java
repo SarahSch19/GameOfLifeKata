fa1a2d130667313310ecec40c413b69f3dd2ca4e
SarahSch19 <grepo.sarah19@gmail.com>
2021-03-24 20:58:58 +0100

    8 tests done

--- java/gameoflife/Grid.java
+++ java/gameoflife/Grid.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Grid {
     private Cell[][] cells;
+    private Cell[][] newCells;
     private int sizeGrid;
     private Random rd;
 
@@ -32,27 +33,39 @@ public class Grid {
 
             }
         }
-
-
-
     }
 
     public void generateNextState() {
         int aliveNeighbours;
+        boolean state ;
+
+        this.newCells = new Cell[this.cells.length][this.cells.length];
+        for (int i = 0; i < this.cells.length; ++i) {
+            for (int j = 0; j < this.cells[i].length; ++j) {
+                this.newCells[i][j] = new Cell(this.cells[i][j].isAlive());
+            }
+        }
+
         for (int i = 0; i < this.cells.length; ++i) {
             for (int j = 0; j < this.cells[i].length; ++j) {
                 aliveNeighbours = this.countNeighboursAlive(i, j) ;
-                this.cells[i][j].setIsAlive(cells[i][j].processState(cells[i][j].isAlive(), aliveNeighbours));
+                state = newCells[i][j].isAlive();
+                state = newCells[i][j].processState(state, aliveNeighbours);
+                this.cells[i][j].setIsAlive(state);
             }
         }
     }
 
-    private int countNeighboursAlive (int cellX, int cellY) {
-        int alive = 0 ;
+    private int countNeighboursAlive (int lin, int col) {
+        int alive = 0, x, y ;
         for (int i = -1 ; i <= 1 ; ++i) {
             for (int j = -1 ; j <= 1 ; ++j) {
-                if (i != 0 && j != 0 && cellX + i >= 0 && cellY + j >= 0 && cellX + i < this.cells.length && cellY + j < this.cells[0].length)
-                    alive = this.cells[cellX + i][cellY + j].isAlive() == true ? 1 : 0;
+                x = lin + i;
+                y = col + j;
+                //condition de vérification des bornes du tableau
+                if (x >= 0 && y >= 0 && x < this.cells.length && y < this.cells[0].length)
+                    if (cells[x][y] != cells[lin][col])
+                        alive += this.newCells[x][y].isAlive() == true ? 1 : 0;
             }
         }
         return alive ;
